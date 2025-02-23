9a4b7b7037b7c754dff8a82a38436474e8c1caf7
Debasish Boidya <119414004+DebasishBoidya@users.noreply.github.com>
2025-02-24 05:15:17 +0530

    Add files via upload

---
+++ HMS/src/main/java/com/dao/UserDao.java
@@ -0,0 +1,157 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.entity.User;
+
+public class UserDao {
+
+	private Connection conn;
+
+	public UserDao(Connection conn) {
+		super();
+		this.conn = conn;
+	}
+	
+	public List<User> getAllUsers() {
+        List<User> list = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM user_dtls";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt(1));
+                u.setFullName(rs.getString(2));
+                u.setEmail(rs.getString(3));
+                u.setPassword(rs.getString(4));
+                u.setRegistrationDate(rs.getDate(5));
+                list.add(u);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+	public boolean register(User u) {
+		boolean f = false;
+
+		try {
+			String sql = "insert into user_dtls(full_name,email,password,registration_date) values(?,?,?,?) ";
+
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, u.getFullName());
+			ps.setString(2, u.getEmail());
+			ps.setString(3, u.getPassword());
+			ps.setDate(4, new java.sql.Date(u.getRegistrationDate().getTime()));
+
+			int i = ps.executeUpdate();
+
+			if (i == 1) {
+				f = true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return f;
+	}
+
+	public User login(String em, String psw) {
+		User u = null;
+
+		try {
+			String sql = "select * from user_dtls where email=? and password=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, em);
+			ps.setString(2, psw);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				u = new User();
+				u.setId(rs.getInt(1));
+				u.setFullName(rs.getString(2));
+				u.setEmail(rs.getString(3));
+				u.setPassword(rs.getString(4));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return u;
+	}
+
+	public boolean checkOldPassword(int userid, String oldPassword) {
+		boolean f = false;
+
+		try {
+			String sql = "select * from user_dtls where id=? and password=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, userid);
+			ps.setString(2, oldPassword);
+
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				f = true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return f;
+	}
+
+	public boolean changePassword(int userid, String newPassword) {
+		boolean f = false;
+
+		try {
+			String sql = "update user_dtls set password=? where id=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, newPassword);
+			ps.setInt(2, userid);
+
+			int i = ps.executeUpdate();
+			if (i == 1) {
+				f = true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return f;
+	}
+
+	public boolean updatePassword(String email, String newPassword) {
+        boolean f = false;
+
+        try {
+            String sql = "update user_dtls set password=? where email=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, newPassword);
+            ps.setString(2, email);
+
+            int i = ps.executeUpdate();
+            if (i == 1) {
+                f = true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+}
